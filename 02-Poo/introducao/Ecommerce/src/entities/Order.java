440496bd2e8b71dfff28f8c8b47bc2ea533c909f
J-Breno <joao.breno85@hotmail.com>
2024-03-01 13:39:11 -0300

    iniciando o Order e OrderItem

--- 02-Poo/introducao/Ecommerce/src/entities/Order.java
+++ 02-Poo/introducao/Ecommerce/src/entities/Order.java
@@ -1,5 +1,59 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
 public class Order {
 
+	private Date momment;
+	private OrderStatus status;
+	
+	private List<OrderItem> itens = new ArrayList<>();
+	
+	public Order() {
+	}
+
+	public Order(Date momment, OrderStatus status) {
+		this.momment = momment;
+		this.status = status;
+	}
+
+	public Date getMomment() {
+		return momment;
+	}
+
+	public void setMomment(Date momment) {
+		this.momment = momment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public List<OrderItem> getItem() {
+		return itens;
+	}
+	
+	public void addItem(OrderItem item) {
+		itens.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		itens.remove(item);
+	}
+	
+	public Double total() {
+		Double sum = 0.0;
+		for(OrderItem item : itens) {
+			sum += item.subTotal();
+		}
+		return sum;
+	}
 }
