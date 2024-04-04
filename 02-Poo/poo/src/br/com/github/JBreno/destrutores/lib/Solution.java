a0e0c973848d2da6c8176396aff8ec2470336c15
J-Breno <joao.breno85@hotmail.com>
2024-04-04 06:46:54 -0300

    Fazendo exercicio da leetcode

---
+++ 02-Poo/poo/src/br/com/github/JBreno/destrutores/lib/Solution.java
@@ -0,0 +1,23 @@
+package br.com.github.JBreno.destrutores.lib;
+
+import java.util.Scanner;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Scanner sc = new Scanner(System.in);
+        int n = 4;
+
+        for(int i = 0; i < n; i++) {
+            if((nums[i] + nums[i+1]) == target) {
+                System.out.printf("[%d,%d]", i, i+1);
+                return new int[] {i, i+1};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] result = solution.twoSum(new int[]{3,3}, 6);
+    }
+}
