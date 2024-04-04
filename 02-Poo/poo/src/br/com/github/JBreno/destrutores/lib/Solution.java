9dc0eb3aebee17e3fc2c4d73d1c2930f5d426e45
J-Breno <joao.breno85@hotmail.com>
2024-04-04 08:30:27 -0300

    Refatorando o exercicio

--- 02-Poo/poo/src/br/com/github/JBreno/destrutores/lib/Solution.java
+++ 02-Poo/poo/src/br/com/github/JBreno/destrutores/lib/Solution.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Scanner sc = new Scanner(System.in);
-        int n = 4;
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < nums.length; i++) {
             if((nums[i] + nums[i+1]) == target) {
                 System.out.printf("[%d,%d]", i, i+1);
                 return new int[] {i, i+1};
