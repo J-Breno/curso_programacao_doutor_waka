814d3fc743cb8927f49e2441a8ddd5f87d641547
J-Breno <joao.breno85@hotmail.com>
2024-04-07 16:21:37 -0300

    .

--- 02-Poo/poo/src/br/com/github/JBreno/destrutores/lib/Solution.java
+++ 02-Poo/poo/src/br/com/github/JBreno/destrutores/lib/Solution.java
@@ -1,11 +1,8 @@
 package br.com.github.JBreno.destrutores.lib;
 
-import java.util.Scanner;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Scanner sc = new Scanner(System.in);
-
         for(int i = 0; i < nums.length; i++) {
             if((nums[i] + nums[i+1]) == target) {
                 System.out.printf("[%d,%d]", i, i+1);
@@ -18,5 +15,6 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] result = solution.twoSum(new int[]{3,3}, 6);
+
     }
 }
